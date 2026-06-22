901e420b226dbaf62252837898a2d09e8efe24da
narendratalent <144669262+narendratalent@users.noreply.github.com>
2026-06-22 17:10:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,39 @@ class Question(BaseModel):
 def query(data: Question):
 
     question = data.question
-
+    print("QUESTION:", question)
     sql_query = get_intent_sql(question.lower())
+    if sql_query:
+        print("INTENT MATCHED")
+        return {"sql": sql_query}
 
-    if not sql_query:
+    print("CALLING OLLAMA")
+    try:
 
         prompt = f"""
-Generate only MySQL query.
+        Generate only MySQL query.
 
-Table: consumer_nlpdata
+        Table: consumer_nlpdata
+        Columns:
+        consumer_no
+        division
+        location_code
+        group_no
+        rd_no
+        consumer_name
+        consumer_status
+        tariff_category
+        address1
+        address2
+        mobile_no
+        net_bill
+        arrear
+        billed_unit
+        """
 
-Question:
-{question}
-"""
+        Question:
+        {question}
+        """
 
         response = chat(
             model='phi3:mini',
@@ -35,12 +55,20 @@ Question:
                 }
             ]
         )
+        print("OLLAMA RESPONSE:", response)
 
         sql_query = response['message']['content']
         sql_query = sql_query.replace("```sql","")
         sql_query = sql_query.replace("```","")
         sql_query = sql_query.strip()
 
-    return {
+        return {
         "sql": sql_query
-    }
+        }
+    except Exception as e:
+
+        print("OLLAMA ERROR:", str(e))
+
+        return {
+            "error": str(e)
+        }
